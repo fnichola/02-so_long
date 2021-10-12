832299e270281ff824d6344a3fb950f09379f802
Flenghi Nicholas foppiano <fnichola@student.42tokyo.jp>
2021-10-12 19:29:32 +0900

    added x button close

--- srcs/hook_example.c
+++ srcs/hook_example.c
@@ -1,5 +1,6 @@
 #include "../mlx/mlx.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct	s_vars {
 	void	*mlx;
@@ -11,6 +12,11 @@ int	key_hook(int keycode, t_vars *vars)
 	(void)vars;
 
 	printf("key_hook: %d\n", keycode);
+	if (keycode == 53)
+	{
+		mlx_destroy_window(vars->mlx, vars->win);
+		exit(EXIT_SUCCESS);
+	}
 	return (0);
 }
 
@@ -22,6 +28,11 @@ int	mouse_hook(int button, int x, int y, t_vars *vars)
 	return (0);
 }
 
+int	close()
+{
+	exit(EXIT_SUCCESS);
+}
+
 int	main(void)
 {
 	t_vars	vars;
@@ -30,6 +41,7 @@ int	main(void)
 	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
 	mlx_key_hook(vars.win, key_hook, &vars);
 	mlx_mouse_hook(vars.win, mouse_hook, &vars);
+	mlx_hook(vars.win, 17, (1L<<17), close, &vars);
 	mlx_loop(vars.mlx);
 	return (0);
 }
